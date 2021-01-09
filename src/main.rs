872e473921075adb3fa4e220db481383cc0b5ec3
levabala <levabala@ya.ru>
2021-01-10 01:55:18 +0300

    splitted realization of comparision and distribution for Point i32 and f32

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 use num::{Float, Integer};
 use rand::distributions::{Distribution, Standard};
-use rand::Rng;
+use rand::{random, Rng};
 use std::cmp::PartialEq;
+
 trait IntFloat: Integer + Float {}
 
-struct Point<T: IntFloat> {
+struct Point<T> {
     x: T,
     y: T,
 }
 
-impl<T: IntFloat> PartialEq for Point<T> {
-    fn eq(&self, p2: &Point<T>) -> bool {
+// TODO: unite comparing and distribution for Point i32 and f32
+impl PartialEq for Point<i32> {
+    fn eq(&self, p2: &Point<i32>) -> bool {
         return self.x == p2.x && self.y == p2.y;
     }
 }
 
-impl<T: IntFloat> Distribution<Point<T>> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point<T> {
+impl PartialEq for Point<f32> {
+    fn eq(&self, p2: &Point<f32>) -> bool {
+        return self.x == p2.x && self.y == p2.y;
+    }
+}
+
+impl Distribution<Point<i32>> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point<i32> {
+        Point {
+            x: rng.gen(),
+            y: rng.gen(),
+        }
+    }
+}
+
+impl Distribution<Point<f32>> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point<f32> {
         Point {
             x: rng.gen(),
             y: rng.gen(),
@@ -25,6 +42,15 @@ impl<T: IntFloat> Distribution<Point<T>> for Standard {
 }
 
 fn main() {
+    let p1 = random::<Point<i32>>();
+    let p2 = random::<Point<i32>>();
+
+    let p3 = random::<Point<f32>>();
+    let p4 = random::<Point<f32>>();
+
+    let c1 = p1 == p2;
+    let c2 = p3 == p4;
+
     // let p1 = Point::generate();
 
     // let p2 = Point::generate();
